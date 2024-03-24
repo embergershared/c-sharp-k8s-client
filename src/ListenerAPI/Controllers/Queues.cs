f6f4038a455bc6ef3fa6f072411894a2742f6b50
Emmanuel <bergerat62@gmail.com>
2024-03-24 15:54:14 -0400

    Added receive/complete 1 message per queue endpoint

--- src/ListenerAPI/Controllers/Queues.cs
+++ src/ListenerAPI/Controllers/Queues.cs
@@ -3,6 +3,8 @@ using Azure.Messaging.ServiceBus;
 using Azure.Messaging.ServiceBus.Administration;
 using ListenerAPI.Classes;
 using ListenerAPI.Constants;
+using ListenerAPI.Helpers;
+using ListenerAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Azure;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ListenerAPI.Controllers
@@ -37,18 +40,52 @@ namespace ListenerAPI.Controllers
       _logger.LogDebug("Controllers/Queues constructed");
     }
 
-    // POST api/<QueuesController>
+    // GET api/queues
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Get()
+    {
+      _logger.LogInformation("HTTP GET /api/Queues called");
+
+      if (!bool.Parse(AppGlobal.Data["IsUsingServiceBus"]))
+        return NotFound("Error: No ServiceBus(es) set in configuration to RECEIVE messages FROM");
+
+      var sbNamespaces = Const.SbNamesKeys
+        .Select(key => _config[key])
+        .Where(sb => !string.IsNullOrEmpty(sb)).ToList();
+
+      var tasks = new List<Task<ReceivedMessage>>();
+      foreach (var sbNamespace in sbNamespaces)
+      {
+        await AddReceiverFromQueuesTasks(sbNamespace!, tasks);
+      }
+
+      var results = await Task.WhenAll(tasks);
+
+      if (!results.All(predicate: x => x.IsSuccessfullyReceived))
+        return StatusCode(StatusCodes.Status500InternalServerError, "Error Receiving messages");
+
+      foreach (var result in results)
+      {
+        var resultJson = JsonSerializer.Serialize(result);
+        _logger.LogInformation("Received message: {content}", resultJson);
+      }
+      return Ok(JsonSerializer.Serialize(results));
+    }
+
+    // POST api/queues
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult> Put([FromBody] int value)
+    public async Task<IActionResult> Put([FromBody] int value)
     {
       _logger.LogInformation("HTTP POST /api/Queues called with {value} in body", value);
 
       if (!bool.Parse(AppGlobal.Data["IsUsingServiceBus"]))
-        return StatusCode(StatusCodes.Status404NotFound,
-          "Error: No ServiceBus(es) set in configuration to send messages to");
+        return NotFound("Error: No ServiceBus(es) set in configuration to SEND messages TO");
 
       var sbNamespaces = new List<string>();
       foreach (var key in Const.SbNamesKeys)
@@ -60,7 +97,7 @@ namespace ListenerAPI.Controllers
         }
       }
 
-      var tasks = new List<Task<ActionResult>>();
+      var tasks = new List<Task<IActionResult>>();
       foreach (var sbNamespace in sbNamespaces)
       {
         await AddSenderToQueuesTasks(value, sbNamespace, tasks);
@@ -75,16 +112,21 @@ namespace ListenerAPI.Controllers
 
       return StatusCode(StatusCodes.Status500InternalServerError, "Error Sending messages");
     }
-
-    private async Task AddSenderToQueuesTasks(int value, string sbName, List<Task<ActionResult>> tasks)
+    
+    #region Private Methods
+    // Send X messages in a batch to all queue(s) in all Service Bus namespace(s)
+    private async Task AddSenderToQueuesTasks(int value, string sbName, List<Task<IActionResult>> tasks)
     {
+      _logger.LogDebug("Queues.AddSenderToQueuesTasks({value}, {sbName}, tasks) started", value, sbName);
+
       var queuesNames = await GetQueueNames(sbName);
       tasks.AddRange(queuesNames.Select(queue =>
-        ActionResultAsync(_sbClientFactory.CreateClient(sbName).CreateSender(queue), value)));
+        SendMessageBatchToQueueAsync(_sbClientFactory.CreateClient(sbName).CreateSender(queue), value)));
     }
-
-    private async Task<ActionResult> ActionResultAsync(ServiceBusSender sender, int value)
+    private async Task<IActionResult> SendMessageBatchToQueueAsync(ServiceBusSender sender, int value)
     {
+      _logger.LogDebug("Queues.SendMessageBatchToQueueAsync({sender}, {value}) started", sender.Identifier, value);
+
       // create a batch to send multiple messages
       using var messageBatch = await sender.CreateMessageBatchAsync();
 
@@ -115,11 +157,57 @@ namespace ListenerAPI.Controllers
       }
     }
 
-    private static async Task<List<string>> GetQueueNames(string serviceBusName)
+    // Receive 1 message from all queue(s) in all Service Bus namespace(s)
+    private async Task AddReceiverFromQueuesTasks(string sbName, List<Task<ReceivedMessage>> tasks)
     {
+      _logger.LogDebug("Queues.AddReceiverFromQueuesTasks({sbName}, tasks) started", sbName);
+
+      var queuesNames = await GetQueueNames(sbName);
+      tasks.AddRange(queuesNames.Select(queue =>
+        ReceiveMessageFromQueueAsync(_sbClientFactory.CreateClient(sbName).CreateReceiver(queue))));
+    }
+    private async Task<ReceivedMessage> ReceiveMessageFromQueueAsync(ServiceBusReceiver receiver)
+    {
+      _logger.LogDebug("Queues.ReceiveMessageFromQueueAsync({receiver}) started", receiver.Identifier);
+      var message = new ReceivedMessage
+      {
+        ServiceBusName = StringHelper.RemoveSbSuffix(receiver.FullyQualifiedNamespace),
+        QueueName = receiver.EntityPath
+      };
+
+      try
+      {
+        {
+          // the received message is a different type as it contains some service set properties
+          var sbReceivedMessage = await receiver.ReceiveMessageAsync();
+
+          message.IsSuccessfullyReceived = true;
+          message.Body = sbReceivedMessage.Body.ToString();
+          message.SeqNumber = sbReceivedMessage.SequenceNumber;
+          message.MessageId = sbReceivedMessage.MessageId;
+
+          // complete the message, thereby deleting it from the service
+          await receiver.CompleteMessageAsync(sbReceivedMessage);
+        }
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError("Called failed with exception: {ex}", ex);
+      }
+
+      return message;
+    }
+
+    // Get all queue(s) for a Service Bus namespace
+    private async Task<List<string>> GetQueueNames(string serviceBusName)
+    {
+      _logger.LogDebug("Queues.GetQueueNames({serviceBusName}) started", serviceBusName);
+
       // Query the available queues for the Service Bus namespace.
       var adminClient = new ServiceBusAdministrationClient
-        ($"{serviceBusName}{Const.SbPublicSuffix}", new DefaultAzureCredential());
+        ($"{serviceBusName}{Const.SbPublicSuffix}",
+          AzureCreds.GetCred(_config.GetValue<string>("PreferredAzureAuth"))
+        );
       var queueNames = new List<string>();
 
       // Because the result is async, the queue names need to be captured
@@ -132,10 +220,12 @@ namespace ListenerAPI.Controllers
 
       return queueNames;
     }
+    #endregion
 
+    // Trace Controller disposal
     protected override void Dispose(bool disposing)
     {
-      _logger.LogDebug("Controllers/Queues disposing");
+      _logger.LogDebug("Queues.Dispose() started");
       if (disposing)
       {
         //sbSender.DisposeClientAsync();
---
+++ src/ListenerAPI/Models/ReceivedMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ListenerAPI.Models
+{
+  public class ReceivedMessage
+  {
+    [JsonIgnore]
+    public bool IsSuccessfullyReceived { get; set; } = false;
+    public long? SeqNumber { get; set; }
+    public string? Body { get; set; }
+    public string? MessageId { get; set; }
+    public string? ServiceBusName { get; set; }
+    public string? QueueName { get; set; }
+  }
+}
